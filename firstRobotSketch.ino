87cc81a942ecaf6a52e71d710e2fc84a9da8cd9f
andrewberp <123495166+andrewberp@users.noreply.github.com>
2024-02-04 22:34:43 -0500

    Update firstRobotSketch.ino

--- firstRobotSketch.ino
+++ firstRobotSketch.ino
@@ -1,1 +1,29 @@
+#include <Pololu3piPlus32U4.h>
+
+using namespace Pololu3piPlus32U4;
+
+Encoders encoders;
+Buzzer buzzer;
+Motors motors;
+ButtonA buttonA;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  delay(1000);
+  buzzer.play("c32");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(buttonA.isPressed()) {
+    motors.setSpeeds(100, 100);
+    Serial.println("Motors running");
+  }
+  else {
+    motors.setSpeeds(0, 0);
+  }
+
+}
 
